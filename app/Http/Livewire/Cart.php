8836e0d7cc6947c93b5d0ef74b33e77bc731bbd2
Siaga Parlindungan Harahap <73106458+siagaharahap@users.noreply.github.com>
2020-12-08 16:15:07 +0700

    Menambah fungsi increase, decrease,  dan remove
    Menambah fungsi increase, decrease,  dan remove item.

--- app/Http/Livewire/Cart.php
+++ app/Http/Livewire/Cart.php
@@ -96,4 +96,50 @@ class Cart extends Component
     public function disableTax(){
         $this->tax = "0%";
     }
+    
+    public function increaseItem($rowId){
+        $idProduct = substr($rowId, 4,5 );
+        $product = ProductModel::find($idProduct);
+
+        $cart = \Cart::session(Auth()->id())->getContent();
+
+        $checkItem = $cart->whereIn('id', $rowId);
+
+        if($product->qty == $checkItem[$rowId]->quantity){
+            session()->flash('error', 'Number of items less than stock');
+        }else{
+            \Cart::session(Auth()->id())->update($rowId, [
+                'quantity' => [
+                    'relative' => true,
+                    'value' => 1
+                ]
+            ]);
+        }
+
+        
+    }
+
+    public function decreaseItem($rowId){
+        $idProduct = substr($rowId, 4,5 );
+        $product = ProductModel::find($idProduct);
+
+        $cart = \Cart::session(Auth()->id())->getContent();
+
+        $checkItem = $cart->whereIn('id', $rowId);
+
+        if($checkItem[$rowId]->quantity == 1){
+            $this->removeItem($rowId);
+        }else{
+            \Cart::session(Auth()->id())->update($rowId, [
+                'quantity' => [
+                    'relative' => true,
+                    'value' => -1
+                ]
+            ]);
+        }
+    }
+
+    public function removeItem($rowId){
+        \Cart::session(Auth()->id())->remove($rowId);
+    }
 }
