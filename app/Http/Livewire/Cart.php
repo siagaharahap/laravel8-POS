e533e0143de567cc7b8014530b87615c45cbcb21
Siaga Parlindungan Harahap <73106458+siagaharahap@users.noreply.github.com>
2020-12-09 16:41:09 +0700

    Update Cart.php

--- app/Http/Livewire/Cart.php
+++ app/Http/Livewire/Cart.php
@@ -5,14 +5,27 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Product as ProductModel;
 use Carbon\Carbon;
+use Livewire\WithPagination;
 
 class Cart extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
     public $tax = "0%";
+
+    public $search;
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
     
     public function render()
     {
-        $products = ProductModel::orderBy('created_at', 'DESC')->get();
+        $products = ProductModel::where('name', 'like', '%'.$this->search.'%')->orderBy('created_at', 'DESC')->paginate(8);
+
 
         $condition = new \Darryldecode\Cart\CartCondition([
             'name' => 'pajak',
@@ -68,13 +81,20 @@ class Cart extends Component
         $cart = \Cart::session(Auth()->id())->getContent();
         $cekItemId = $cart->whereIn('id', $rowId);
 
+        $idProduct = substr($rowId, 4,5 );
+        $product = ProductModel::find($idProduct);
+
         if($cekItemId->isNotEmpty()){
-            \Cart::session(Auth()->id())->update($rowId, [
-                'quantity' => [
-                    'relative' => true,
-                    'value' => 1
+            if($product->qty == $cekItemId[$rowId]->quantity){
+                session()->flash('error', 'Number of items less than stock');
+            }else{
+                \Cart::session(Auth()->id())->update($rowId, [
+                    'quantity' => [
+                        'relative' => true,
+                        'value' => 1
                 ]
             ]);
+            }
         }else{
             $product = ProductModel::findOrFail($id);
             \Cart::session(Auth()->id())->add([
@@ -96,7 +116,6 @@ class Cart extends Component
     public function disableTax(){
         $this->tax = "0%";
     }
-    
     public function increaseItem($rowId){
         $idProduct = substr($rowId, 4,5 );
         $product = ProductModel::find($idProduct);
